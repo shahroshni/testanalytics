2d0430337ba79cb59254fa0eb4fbb8674fe2ef87
shahroshni <81532338+shahroshni@users.noreply.github.com>
2021-03-29 15:56:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,79 @@
 var imagename = ""
+var days31 = [1,3,5,7,8,10,12]
+var days30 = [4,6,9,11]
+function change_year()
+{
+    let x = document.getElementById("year");
+    let d = new Date();
+    for (var i = 1930, j=1;i<=d.getFullYear();i++, j++)
+    {
+      var option = document.createElement("option");
+      option.text = i;
+      option.value = j;
+      option.id = "yearoption"
+      x.add(option)
+    }
+}
+function change_day()
+{
+  //get selected month
+  let m = document.getElementById("month").selectedIndex;
+  //get selected month's value
+  let y =parseInt(document.getElementsByTagName("option")[m].value);
+  d = document.getElementById("day");
+  //check for month having 31 days
+    if(days31.includes(y))
+      {
+        for (i = 1;i<=31;i++){
+          let option = document.createElement("option");
+           option.text = i;
+           d.add(option)
+        }
+      }
+    //check for month not having 31 days
+    else{
+          //check for month having 30 days
+          if(days30.includes(y)){
+              for (i = 1;i<=30;i++){
+              let option = document.createElement("option");
+               option.text = i;
+               d.add(option)
+               }
+            }
+          //check for Feb
+          else{
+              let leap;
+              let x = document.getElementById("year").selectedIndex;
+              //get selected year's value
+              let y =parseInt((document.querySelectorAll("#yearoption")[x].text));
+              //check if year is a leap year or not
+                if (y % 4 == 0)
+                {
+                  if (y % 100 == 0) {
+                      if (y % 400 == 0){leap=1;}
+                      else{leap=0;}}
+                  else
+                    leap=1;
+                }
+                else{leap=0}
+          if (leap==1)
+          {
+            for (i = 1;i<=29;i++)
+            {
+              let option = document.createElement("option");
+               option.text = i;
+               d.add(option)
+             }
+          }
+          else{
+            for (i = 1;i<=28;i++){
+              let option = document.createElement("option");
+               option.text = i;
+               d.add(option)}
+          }
+        }
+    }
+}
 function sendimage()
 {
     imagename = document.querySelector('input[name = car]:checked + label>img').getAttribute("src")
